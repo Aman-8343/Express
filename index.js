28ac9b57feff352865467babfcd2e361c0f9983b
Aman kumar <amankumar105541@gmail.com>
2025-03-09 07:03:29 +0530

    creating middleware

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+// const middleware=(req,res,next)=>{
+//   console.log(req)
+// }
+
+app.use(express.static(path.join(__dirname,"public")))
+// app.use(middleware)
+// next()
+
+app.get('/hello/:name', (req, res) => {
+  res.send('Hello world'+req.params.name)
 })
 
 app.get('/about', (req, res) => {
-    res.send('About!')
-  })
+  res.sendFile(path.join(__dirname,'index.html'))
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
